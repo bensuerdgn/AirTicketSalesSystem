d1380a880aef8c688f280440fef1064bd70560e8
Bensu Erdoğan <bensuerdogan58@gmail.com>
2021-12-13 13:46:13 +0300

    initial commit

---
+++ src/main/java/com/airticketsalessystem/services/TicketService.java
@@ -0,0 +1,14 @@
+package com.airticketsalessystem.services;
+
+import com.airticketsalessystem.model.Admin;
+import com.airticketsalessystem.model.Ticket;
+
+import java.util.List;
+
+public interface TicketService {
+    public List<Ticket> getAllTicket();
+    public Ticket getTicketById(int id);
+    public void addTicket(Ticket ticket);
+    public void deleteTicket(int id);
+    public List<Ticket> getByUserIdTickets(int userid);
+}
